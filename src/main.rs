06f6f15ef548b48414a7e1449c581dd68d081236
TincoNomad <rtincopa@proton.me>
2023-07-28 20:44:49 -0500

    mi primer commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template, context};
+use rocket::fs::NamedFile;
+use std::path::{Path, PathBuf};
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context!{
+        title: "Renzo Tincopa Barreto"
+    })
+}
+
+#[get("/static/<file..>")]
+async fn static_files(file: PathBuf) -> Option<NamedFile> {
+    let path = Path::new("static/").join(file);
+    if let Ok(file) = NamedFile::open(path).await {
+        Some(file)
+    } else {
+        None
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index, static_files])
+    .attach(Template::fairing())
+}
